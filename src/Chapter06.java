9e73ddf19b268670e1f56eeac9806481280d408c
hiroki.nakamura <125346034+hiroki-coffee@users.noreply.github.com>
2023-02-14 23:52:08 +0900

    Chpter6

---
+++ src/Chapter06.java
@@ -0,0 +1,23 @@
+
+public class Chapter06 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+long L = 10000000000L;
+System.out.println(L);
+double π =3.14;
+System.out.println(π);
+boolean 正 =true;
+System.out.println(正);
+Integer A =100;
+System.out.println(A);
+Float B = 1.4142f;
+System.out.println(B);
+Character C ='う';
+System.out.println(C);
+int [] arreay = {0, 10, 20, 30, 40};
+System.out.println(arreay[3]);
+
+	}
+
+}
